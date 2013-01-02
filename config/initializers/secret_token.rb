b263309a8ba1857b8b9060a69ba5d3b7314bfdd4
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-01-02 09:53:56 -0800

    Base Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrosswordR::Application.config.secret_token = '03a9bf6399bebba6698b1bb8ab6abd99ff5ad8c8f424ea96864684f29b3d284a5e5b40da5d70aeb8bf4bde3cbe523f1640a50ac7e17165d842da09a61c6cfbf2'
