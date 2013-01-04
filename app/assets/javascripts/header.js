c08af45e985090fbf628cc231c357b95032461a8
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-01-04 13:58:01 -0800

    Admin-only menu and fixed menu flicker

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){	
-	$('.menuTab').on('hover', function(){
+	$('.menuTab').on('mouseenter', function(){
 		$(this).find('.dropMenu').fadeIn(60);
 	});
 	$('.menuTab').on('mouseleave', function(){
-		$(this).find('.dropMenu').fadeOut(200);
+		$(this).find('.dropMenu').fadeOut(50);
 	});
-	$('.dropMenu li').on('hover', function(){
+	$('.dropMenu li').on('mouseenter', function(){
 		$(this).find('.leftMenu').fadeIn(60);
 	});
 	$('.dropMenu li').on('mouseleave', function(){
-		$(this).find('.leftMenu').fadeOut(200);
+		$(this).find('.leftMenu').fadeOut(50);
 	});
 });
