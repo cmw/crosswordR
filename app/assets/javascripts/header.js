81c5f436db71e16a00e4d958af2180dc774666f9
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-01-04 14:37:33 -0800

    Removed the erroneous stop triggers

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){	
 	$('.menuTab').on('mouseenter', function(){
-		$(this).find('.dropMenu').stop().fadeIn(60);
+		$(this).find('.dropMenu').fadeIn(60);
 	});
 	$('.menuTab').on('mouseleave', function(){
-		$(this).find('.dropMenu').stop().fadeOut(50);
+		$(this).find('.dropMenu').fadeOut(50);
 	});
 	$('.dropMenu li').on('mouseenter', function(){
-		$(this).find('.leftMenu').stop().fadeIn(60);
+		$(this).find('.leftMenu').fadeIn(60);
 	});
 	$('.dropMenu li').on('mouseleave', function(){
-		$(this).find('.leftMenu').stop().fadeOut(50);
+		$(this).find('.leftMenu').fadeOut(50);
 	});
 });
