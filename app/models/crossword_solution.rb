c1777f1ef00c699df9b3d7350e37389b5b570686
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-01-17 05:50:47 -0500

    No-repeat quote, random puzzle improvements, % completion

--- app/models/crossword_solution.rb
+++ app/models/crossword_solution.rb
@@ -2,5 +2,12 @@ class CrosswordSolution < ActiveRecord::Base
 	belongs_to :user, :counter_cache => true
 	belongs_to :crossword_puzzle, :counter_cache => true
 	
-  attr_accessible :crossword_puzzle_id, :solution_complete, :solution_letters, :user_id
+ 	attr_accessible :crossword_puzzle_id, :solution_complete, :solution_letters, :user_id
+
+ 	def completion_percentage
+ 		sol_letters = self.solution_letters.gsub("_","")
+ 		puz_letters = self.crossword_puzzle.letters.gsub("_","")
+ 		return ((sol_letters.length.to_f)/(puz_letters.length.to_f)*100).round(1)
+ 	end
+
 end
