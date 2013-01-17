719c853906e13d50de65a0cd18218766ceb85367
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-01-17 04:07:43 -0500

    Stats, Random Puzzle, Puzzle of the Day on Home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,9 +50,16 @@ class User < ActiveRecord::Base
   def encrypt_password
   	if password.present?
   	  self.password_salt = BCrypt::Engine.generate_salt
-	  self.password_hash = BCrypt::Engine.hash_secret(password, password_salt) 		
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt) 		
   	end
   end
   
+  def rand_owned_puzzle
+    CrosswordPuzzle.where('user_id = ?', self.id).order("RANDOM()").first
+  end
+
+  def rand_unowned_puzzle
+    CrosswordPuzzle.where('user_id != ?', self.id).order("RANDOM()").first
+  end
   
 end
